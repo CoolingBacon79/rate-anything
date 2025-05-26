8d7816b14a1f9df2fb8aaab2ec2fa8a43c196f92
Alvin Haraldsson <alvin.haraldsson@gmail.com>
2025-05-27 01:22:22 +0200

    Fix responsiv design för mobil och fyll hela skärmen2

--- src/MainApp.jsx
+++ src/MainApp.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { supabase } from './supabaseClient'
+import './index.css';
 
 export default function MainApp({ user }) {
   const [items, setItems] = useState([])
